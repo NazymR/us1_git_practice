a2c6bc9da9f481f57c581dca8963cd8ce5074bfd
Nazym R <naz2506r@gmail.com>
2022-08-23 16:40:59 -0400

    Hello word.java is updated

--- src/Day1/HelloWord.java
+++ src/Day1/HelloWord.java
@@ -3,5 +3,8 @@ package Day1;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Good old day");
+
+        // some more work here
+        System.out.println("salem");
     }
 }
